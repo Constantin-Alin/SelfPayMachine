c15285c2d70ba236ffd4097761965c4509ee3d57
Constantin-Alin <clmalex99@gmail.com>
2025-01-06 11:35:07 +0100

    Add change money repetition

--- src/Machine.java
+++ src/Machine.java
@@ -17,26 +17,33 @@ public class Machine {
 
         System.out.println("Your change is: " + change);
 
-        if (change > 200) {
+        while (change > 200) {
             System.out.println("200 lei");
+            change -= 200;
         }
-        if (change > 100) {
+        while (change > 100) {
             System.out.println("100 lei");
+            change -= 100;
         }
-        if (change > 50) {
+        while (change > 50) {
             System.out.println("50 lei");
+            change -= 50;
         }
-        if (change > 20) {
+        while (change > 20) {
             System.out.println("20 lei");
+            change -= 20;
         }
-        if (change > 10) {
+        while (change > 10) {
             System.out.println("10 lei");
+            change -= 10;
         }
-        if (change > 5) {
+        while (change > 5) {
             System.out.println("5 lei");
+            change -= 5;
         }
-        if (change > 1) {
+        while (change > 1) {
             System.out.println("1 leu");
+            change -= 1;
         }
     }
 }
